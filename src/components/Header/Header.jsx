a22422ae1f3572e2d9d47f14305c594299635a33
Manoj Kumar Munda <manojkrmunda828@gmail.com>
2024-07-13 23:04:58 +0530

    fixed header responsiveness

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,19 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import Logo from "./Logo";
 import Searchbar from "./Searchbar";
 import ProfileCircle from "./ProfileCircle";
+import { IoIosSearch } from "react-icons/io";
 import { RxHamburgerMenu } from "react-icons/rx";
+import { FaArrowLeftLong } from "react-icons/fa6";
+import { cn } from "../../utils/cn";
 
 const Header = () => {
+  const [showSearchBar, setShowSearchBar] = useState(false);
   return (
     <header className="sticky top-0 bg-white py-2 z-10">
-      <div className="flex justify-between items-center px-4">
-        <div className="inline-flex items-center gap-4">
-          <RxHamburgerMenu size={24} />
+      <div className="flex justify-between items-center px-2 sm:px-4 ">
+        <div
+          className={cn(
+            "inline-flex items-center gap-4 max-w-56 w-full",
+            showSearchBar && "hidden"
+          )}
+        >
+          <button className="sm:hidden">
+            <RxHamburgerMenu size={24} />
+          </button>
           <Logo />
         </div>
+        {showSearchBar && (
+          <div className="grow flex gap-1 items-center">
+            <button onClick={() => setShowSearchBar(false)} className="hover:bg-gray-200 inline-flex justify-center items-center w-12 h-12 rounded-full text-center">
+              <FaArrowLeftLong size={20} />
+            </button>
+            <Searchbar className="flex md:hidden" />
+          </div>
+        )}
         <Searchbar />
-        <ProfileCircle className="w-12 h-12" />
+
+        <div
+          className={cn(
+            "flex gap-4 items-center",
+            showSearchBar && "hidden"
+          )}
+        >
+          <button
+            onClick={() => setShowSearchBar(true)}
+            className="md:hidden hover:bg-gray-200 inline-flex justify-center items-center w-12 h-12 rounded-full text-center"
+          >
+            <IoIosSearch size={24} />
+          </button>
+          <ProfileCircle className="w-12 h-12" />
+        </div>
       </div>
     </header>
   );
--- src/components/Header/Searchbar.jsx
+++ src/components/Header/Searchbar.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 import { IoIosSearch } from "react-icons/io";
-const Searchbar = () => {
+import { cn } from "../../utils/cn";
+const Searchbar = ({ className = "" }) => {
   return (
-    <div className="flex pl-4 py-2 justify-center border shadow-md border-gray-300 max-w-[512px] grow rounded-3xl group">
+    <div
+      className={cn(
+        "mx-2 hidden md:flex pl-4 py-2 justify-center border shadow-md border-gray-300 max-w-[512px] grow rounded-3xl group",
+        className
+      )}
+    >
       <div className="w-full grow">
-        <input type="text" className="outline-none w-full pl-2 font-Roboto" placeholder="Search" />
+        <input
+          type="text"
+          className="outline-none w-full pl-2 font-Roboto"
+          placeholder="Search"
+        />
       </div>
       <button className="pr-4">
         <IoIosSearch size={20} />
