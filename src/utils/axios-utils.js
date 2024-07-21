8ab9c3a69470e2894ef2e620eccb4cdf8d73509a
Manoj Kumar Munda <manojkrmunda828@gmail.com>
2024-07-22 01:08:51 +0530

    axios interceptor added to handle accessToken expiry

--- src/utils/axios-utils.js
+++ src/utils/axios-utils.js
@@ -2,13 +2,31 @@ import axios from "axios";
 
 const client = axios.create({ baseURL: "/api/v1" });
 
+client.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  async (error) => {
+    if (error?.response.status === 401) {
+      await axios
+        .post("/api/v1/users/refresh-token", {
+          withCredentials: true,
+        })
+        .catch((refrehTokenAPIError) => {
+          return Promise.reject(refrehTokenAPIError);
+        });
+      return axios(error.config);
+    }
+
+    return Promise.reject(error);
+  }
+);
+
 export const request = async ({ ...options }) => {
   const onSuccess = (response) => {
     return response;
   };
-  const onError = (error) => {
-    //optionally catch errors and add additional logging here
-    console.log(error);
+  const onError = async (error) => {
     return error;
   };
   return client(options).then(onSuccess).catch(onError);
