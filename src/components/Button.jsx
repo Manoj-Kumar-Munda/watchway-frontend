8bfce97ca78ef9eab1b0a3d8c10621e479df41a2
Manoj Kumar Munda <manojkrmunda828@gmail.com>
2024-07-22 21:34:41 +0530

    created a separate component for channel page btns

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,23 @@ import { cn } from "../utils/cn";
 
 const Button = ({ children, className = "", ...props }) => {
   return (
-    <button className={cn("py-2 px-4 text-center rounded", className)} {...props}>
+    <button
+      className={cn("py-2 px-4 text-center rounded", className)}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+};
+
+export const ChannelBtn = ({ children, className, ...props }) => {
+  return (
+    <button
+      className={cn(
+        "py-2 px-4 text-center rounded bg-themered-500 inline-flex items-center gap-2 w-24 relative before:content-[''] before:absolute before:w-full before:h-full before:bg-themered-300 before:rounded before:-z-10 before:left-1.5 before:-bottom-1.5 "
+      )}
+      {...props}
+    >
       {children}
     </button>
   );
--- src/components/Channel/Header/ChannelHeader.jsx
+++ src/components/Channel/Header/ChannelHeader.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import CoverPic from "./CoverPic";
-import { MdModeEditOutline } from "react-icons/md";
 import ProfileCircle from "../../Header/ProfileCircle";
 import { useNavigate } from "react-router-dom";
-import Button from "../../Button";
+import Button, { ChannelBtn } from "../../Button";
+import { FiEdit2 } from "react-icons/fi";
 
 const ChannelHeader = ({ isMyChannel, channelInfo }) => {
   console.log(channelInfo);
@@ -29,7 +29,9 @@ const ChannelHeader = ({ isMyChannel, channelInfo }) => {
             className="w-16 h-16 md:w-32 md:h-32 border bg-black"
           />
           <div className="flex flex-col">
-            <h2 className="text-sm md:text-xl font-Roboto font-semibold">{channelName}</h2>
+            <h2 className="text-sm md:text-xl font-Roboto font-semibold">
+              {channelName}
+            </h2>
             <span className="text-sm md:text-base">@{username}</span>
 
             <div className="inline-flex gap-2 items-center text-sm md:text-base ">
@@ -42,13 +44,10 @@ const ChannelHeader = ({ isMyChannel, channelInfo }) => {
 
         <div>
           {isMyChannel ? (
-            <Button
-              className="bg-themered-500 inline-flex items-center gap-2 w-24 relative before:content-[''] before:absolute before:w-full before:h-full before:bg-themered-300 before:rounded before:-z-10 before:left-1.5 before:-bottom-1.5"
-              onClick={() => navigate("./edit")}
-            >
-              <MdModeEditOutline />
-              <span className="text-white font-Roboto">Edit</span>
-            </Button>
+            <ChannelBtn onClick={() => navigate("./edit")}>
+              <FiEdit2 color="white" />
+              <span className="text-white font-Roboto font-semibold">Edit</span>
+            </ChannelBtn>
           ) : (
             <button>Subscribe</button>
           )}
