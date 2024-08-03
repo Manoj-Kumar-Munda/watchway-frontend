1796e6b5e9cf6e4fbaa122d018330008b1cb4aeb
Manoj Kumar Munda <manojkrmunda828@gmail.com>
2024-08-03 22:37:43 +0530

    created a hook to fetch all videos to be dispalyed on homepage

---
+++ src/hooks/useVideos.js
@@ -0,0 +1,13 @@
+import { useQuery } from "@tanstack/react-query";
+import { request } from "../utils/axiosConfig";
+
+const fetchVideos = async () => {
+  return await request({ url: `/videos`, method: "get" });
+};
+export const useVideos = () => {
+  return useQuery({
+    queryKey: ["videos"],
+    queryFn: fetchVideos,
+    staleTime: 2 * 60 * 1000,
+  });
+};
