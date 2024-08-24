fbec2724523e128710a202e0a59ee3c4d9477ea3
Manoj Kumar Munda <manojkrmunda828@gmail.com>
2024-08-24 22:41:48 +0530

    create Ui for Tweet component

--- src/pages/Channel/Tweet.jsx
+++ src/pages/Channel/Tweet.jsx
@@ -3,6 +3,11 @@ import { useSelector } from "react-redux";
 import CreateTweet from "../../components/CreateTweet";
 import { useQuery } from "@tanstack/react-query";
 import { request } from "../../utils/axiosConfig";
+import NoTweetImg from "../../assets/svg/notweet.svg";
+import NoData from "../../components/errorPages/NoData";
+import { BiLike } from "react-icons/bi";
+import { BiSolidLike } from "react-icons/bi";
+import { cn } from "../../utils/cn";
 
 const fetchChannelTweets = async (channelId) => {
   if (!channelId) {
@@ -21,8 +26,6 @@ const Tweets = () => {
     queryFn: () => fetchChannelTweets(currentChannel?._id),
   });
 
-  console.log(status);
-  console.log(data);
 
   return (
     <div className="my-4">
@@ -30,13 +33,34 @@ const Tweets = () => {
         {isAuthorized && <CreateTweet />}
       </div>
 
-      <div className="min-h-20 grid place-content-center">
+      <div className={cn(`min-h-48 my-4 font-Roboto border rounded-lg`, data?.data?.length>0 && "min-h-max" )}>
         {data?.data.length === 0 ? (
-          <span className="font-Poppins text-sm">No tweets available</span>
+          <NoData imgSrc={NoTweetImg} message="No tweets" />
         ) : (
-          data?.data.map((tweet) => {
-            
-          })
+          data?.data.map((tweet) => (
+            <div key={tweet?._id} className="p-3 w-full">
+              <div className="flex gap-2">
+                <div className="w-16 h-16 rounded-full bg-gray-600">
+                  <img src={tweet?.owner?.avatar} alt="image" />
+                </div>
+                <div className="flex flex-col gap-3">
+                  <div>
+                    <span className="font-medium">
+                      {tweet?.owner?.fullName}
+                    </span>
+                    <p>{tweet?.content}</p>
+                  </div>
+
+                  <div className="inline-flex gap-1 items-center">
+                    {
+                      tweet?.isLiked ? <BiSolidLike /> : <BiLike />
+                    }
+                    <span className="text-sm">{tweet?.likeCount}</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))
         )}
       </div>
     </div>
