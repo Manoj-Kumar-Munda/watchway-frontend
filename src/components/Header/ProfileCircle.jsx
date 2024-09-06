3b967fc183137d88e75a1536cf996147a81ae88d
Manoj Kumar Munda <manojkrmunda828@gmail.com>
2024-09-06 14:37:32 +0530

    profile icon ui fixed

--- src/components/Header/ProfileCircle.jsx
+++ src/components/Header/ProfileCircle.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import { cn } from "../../utils/cn";
 const ProfileCircle = ({ imgSrc = "", className = "" }) => {
   return (
-    <div className={cn("w-8 h-8 rounded-full overflow-hidden object-cover object-center", className)}>
-      <img src={imgSrc} alt="Profile" className="w-full h-full" />
+    <div className={cn("w-8 h-8 rounded-full overflow-hidden", className)}>
+      <img src={imgSrc} alt="Profile" className="w-full h-full object-cover object-center" />
     </div>
   );
 };
