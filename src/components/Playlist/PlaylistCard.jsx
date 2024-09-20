954482351b70b5dd3368b1b31e6556e3fbfbb739
Manoj Kumar Munda <manojkrmunda828@gmail.com>
2024-09-20 23:23:41 +0530

    created PlaylistCard component

---
+++ src/components/Playlist/PlaylistCard.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { FaVideo } from 'react-icons/fa'
+import { TbPlaylist } from 'react-icons/tb'
+
+const PlaylistCard = ({playlist}) => {
+  return (
+    <div className="border aspect-video w-full h-full relative">
+    {playlist?.cover ? (
+      <img src={playlist?.cover} alt="cover" />
+    ) : (
+      <div className="flex justify-center items-center w-full h-full">
+        No cover image
+      </div>
+    )}
+
+    <div className="absolute bottom-0 right-0 left-0 py-2.5 px-2 flex justify-between items-center bg-white/10 backdrop-blur-lg">
+      <div className="flex items-center gap-1">
+        <TbPlaylist />
+        <span className="font-Poppins text-sm ">
+          {playlist?.name}
+        </span>
+      </div>
+
+      <div className="flex items-center gap-1">
+        <FaVideo />
+        <span className="font-Poppins text-sm ">
+          {playlist?.totalVideos}
+        </span>
+      </div>
+    </div>
+  </div>
+  )
+}
+
+export default PlaylistCard
--- src/pages/Channel/Playlists.jsx
+++ src/pages/Channel/Playlists.jsx
@@ -3,6 +3,7 @@ import { useSelector } from "react-redux";
 import useGetPlaylist from "../../hooks/useGetPlaylists";
 import NoData from "../../components/errorPages/NoData";
 import NoDataLayout from "../../components/errorPages/NoDataLayout";
+import PlaylistCard from "../../components/Playlist/PlaylistCard";
 
 const Playlists = () => {
   const { user } = useSelector((store) => store.auth);
@@ -10,15 +11,19 @@ const Playlists = () => {
 
   if (status === "loading") return <h1>Loading...</h1>;
   if (status === "error") return <h1>Error</h1>;
-  console.log(data);
-  
 
   return (
-    <div>
-      {data?.data?.length === 0 && (
+    <div className="min-h-56 w-full h-full px-2 my-1">
+      {data?.data?.length === 0 ? (
         <NoDataLayout>
           <NoData message="No playlist created" />
         </NoDataLayout>
+      ) : (
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 ">
+          {data?.data?.map((playlist) => (
+            <PlaylistCard playlist={playlist} key={playlist?._id}/>
+          ))}
+        </div>
       )}
     </div>
   );
