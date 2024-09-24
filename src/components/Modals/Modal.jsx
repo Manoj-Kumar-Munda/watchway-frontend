a7e9af44e86b2ef518c99c457a0682490f1b9693
Manoj Kumar Munda <manojkrmunda828@gmail.com>
2024-09-24 18:29:46 +0530

    added ids to modals

--- src/components/Modals/Modal.jsx
+++ src/components/Modals/Modal.jsx
@@ -1,10 +1,28 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useEffect, useRef } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { cn } from "../../utils/cn";
 import { motion, AnimatePresence } from "framer-motion";
+import { closeModal } from "../../store/slices/modalsSlice";
 
 const Modal = ({ modalId, children }) => {
   const isOpen = useSelector((store) => store.modals[modalId]);
+
+  const modalRef = useRef();
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const handler = (event) => {
+      if (!modalRef.current || modalRef.current.contains(event.target)) {
+        return;
+      }
+      if (!modalRef.current.contains(event.target)) {
+        dispatch(closeModal(modalId));
+      }
+    };
+    document.addEventListener("click", handler, true);
+    return () => {
+      document.removeEventListener("click", handler);
+    };
+  }, []);
   return (
     <div
       className={cn(
@@ -18,8 +36,8 @@ const Modal = ({ modalId, children }) => {
             initial={{ opacity: 0, scale: 0.8 }}
             animate={{ opacity: 1, scale: 1 }}
             exit={{ opacity: 0, scale: 0.8 }}
-            className="w-full flex justify-center"
-        
+            className="max-w-screen-sm w-full"
+            ref={modalRef}
           >
             {children}
           </motion.div>
