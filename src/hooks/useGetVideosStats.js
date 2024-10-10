d132ae79340763234eb37c6805cd215e17b3f673
Manoj Kumar Munda <manojkrmunda828@gmail.com>
2024-10-10 22:54:53 +0530

    created useGetVideosStats hook && admin dashboard

---
+++ src/hooks/useGetVideosStats.js
@@ -0,0 +1,15 @@
+import { useQuery } from "@tanstack/react-query";
+import { request } from "../utils/axiosConfig";
+
+const getVideos = async () => {
+  return request({ url: "/dashboard/videos", method: "GET" });
+};
+
+const useGetVideosStats = () => {
+  return useQuery({
+    queryKey: ["videos-stats"],
+    queryFn: getVideos,
+  });
+};
+
+export default useGetVideosStats;
